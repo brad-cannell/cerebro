6eff9d941f9a6c4bf3f09af806ce469f3b73c98b
Brad Cannell <brad.cannell@gmail.com>
2020-07-16 15:46:46 -0500

    Updated names to match X-men names

--- example_data.R
+++ example_data.R
@@ -3,6 +3,8 @@
 # 2019-04-21
 # =============================================================================
 
+library(dplyr)
+
 # browseURL("https://en.wikipedia.org/wiki/List_of_X-Men_members")
 # browseURL("http://www.factacular.com/subjects/Famous_Fictional_Addresses")
 
@@ -47,33 +49,30 @@ cerebro_data <- bind_rows(
   # Person with multiple records in the data, but no typos or errors
   create_person(2, "Robert", "Drake", 1968, 07, 01, 1001, "Cemetery Ridge", "m", 2),
   
-  
-  ## Did here down on the airplane. Go back and change names to mutant names....
-  
   # Person with multiple records in the data and misspelled first name
-  create_person(3, "Bill", "Mason", 1974, 10, 12, 2536, "Dominy", "m", 1),
-  create_person(3, "Bil", "Mason", 1974, 10, 12, 2536, "Dominy", "m", 1),
+  create_person(3, "Warren", "Worthington", 1974, 10, 12, 2536, "Dominy", "m", 1),
+  create_person(3, "Waren", "Worthington", 1974, 10, 12, 2536, "Dominy", "m", 1),
   
   # Person with multiple records in the data and misspelled first and last name
-  create_person(4, "Stacy", "Martin", 1970, 08, 03, 1504, "Rosedale", "f", 1),
-  create_person(4, "Stacey", "Marin", 1970, 08, 03, 1504, "Rosedale", "f", 1),
+  create_person(4, "Sean", "Cassidy", 1970, 08, 03, 1504, "Rosedale", "m", 1),
+  create_person(4, "Shawn", "Casidy", 1970, 08, 03, 1504, "Rosedale", "m", 1),
   
   # Person with multiple records in the data and middle name was incorrectly 
   # entered instead of first name
-  create_person(5, "Amber", "Stevens", 1965, 03, 12, 633, "Wedgewood", "f", 1),
-  create_person(5, "Laura", "Stevens", 1965, 03, 12, 633, "Wedgewood", "f", 1),
+  create_person(5, "Jean", "Grey", 1965, 03, 12, 633, "Wedgewood", "f", 1),
+  create_person(5, "Elaine", "Grey", 1965, 03, 12, 633, "Wedgewood", "f", 1),
   
   # First and last name switched
-  create_person(6, "Anthony", "Arnold", 1967, 05, 18, 222, "Henderson", "m", 1),
-  create_person(6, "Arnold", "Anthony", 1967, 05, 18, 222, "Henderson", "m", 1),
+  create_person(6, "Kevin", "Sydney", 1967, 05, 18, 222, "Henderson", "m", 1),
+  create_person(6, "Sydney", "Kevin", 1967, 05, 18, 222, "Henderson", "m", 1),
   
   # Person with multiple records in the data and Incorrect birth month
-  create_person(7, "Ashley", "Booker", 1971, 05, 23, 1209, "Texas", "f", 1),
-  create_person(7, "Ashley", "Booker", 1971, 06, 23, 1209, "Texas", "f", 1),
+  create_person(7, "Lorna", "Dane", 1971, 05, 23, 1209, "Texas", "f", 1),
+  create_person(7, "Lorna", "Dane", 1971, 06, 23, 1209, "Texas", "f", 1),
   
   # Person with multiple records in the data and Incorrect birth month and year
-  create_person(8, "James", "Gillis", 1960, 02, 02, 115, "Pennsylvania", "m", 1),
-  create_person(8, "James", "Gillis", 1970, 03, 02, 115, "Pennsylvania", "m", 1),
+  create_person(8, "Calvin", "Rankin", 1960, 02, 02, 115, "Pennsylvania", "m", 1),
+  create_person(8, "Calvin", "Rankin", 1970, 03, 02, 115, "Pennsylvania", "m", 1),
   
   # Similar name and dob to existing person that we want to classify as a non-match.
   create_person(9, "Scott", "Sumner", 1968, 04, 01, 1800, "Ocean", "m", 1),
@@ -86,36 +85,36 @@ cerebro_data <- bind_rows(
   create_person(13,"Aleta", "Johnson", 1965, 07, 04, 1610, "Shady Oaks", "f", 1),
   
   # Husband and wife - should be non-match
-  create_person(14, "Robert", "Bowhay", 1967, 09, 13, 419, "Bay", "m", 1),
-  create_person(15, "Brittany", "Bowhay", 1969, 01, 16, 419, "Bay", "f", 1),
+  create_person(14, "Armando", "Muñoz", 1967, 09, 13, 419, "Bay", "m", 1),
+  create_person(15, "Brittany", "Muñoz", 1969, 01, 16, 419, "Bay", "f", 1),
   
   # Missing address - no matches
-  create_person(16, "Latonia", "Reeves", 1971, 02, 06, NA, NA, "f", 1),
+  create_person(16, "Suzanne", "Chan", 1971, 02, 06, NA, NA, "f", 1),
   
   # Missing address - matches
-  create_person(17, "Mark", "Schuler", 1973, 11, 29, 817, "Parker", "m", 1),
-  create_person(17, "Mark", "Schuler", 1974, 11, 29, NA, NA, "m", 1),
+  create_person(17, "Gabriel", "Summers", 1973, 11, 29, 817, "Parker", "m", 1),
+  create_person(17, "Gabriel", "Summers", 1974, 11, 29, NA, NA, "m", 1),
   
   # Missing partial DOB - no matches
-  create_person(18, "Ben", "Graves", 1963, 06, NA, 3434, "Orange", "m", 1),
+  create_person(18, "Kurt", "Wagner", 1963, 06, NA, 3434, "Orange", "m", 1),
   
   # Missing partial DOB - matches
-  create_person(19, "Lauren", "Hall", 1979, 04, 09, 1650, "8th", "f", 1),
-  create_person(19, "Lauren", "Hall", 1979, 04, NA, 1650, "8th", "f", 1),
+  create_person(19, "Katherine", "Pryde", 1979, 04, 09, 1650, "8th", "f", 1),
+  create_person(19, "Katherine", "Pryde", 1979, 04, NA, 1650, "8th", "f", 1),
   
   # Missing full DOB - no matches
- create_person(20, "Peter", "Stahr", NA, NA, NA, 711, "Willow", "m", 1),
+  create_person(20, "Shiro", "Yoshida", NA, NA, NA, 711, "Willow", "m", 1),
   
   # Missing full DOB - matches
-  create_person(21, "Michelle", "Garcia", 1966, 10, 25, 2222, "Lake", "f", 1),
-  create_person(21, "Michelle", "Garcia", NA, NA, NA, 2222, "Lake", "f", 1),
+  create_person(21, "Ororo", "Munroe", 1966, 10, 25, 2222, "Lake", "f", 1),
+  create_person(21, "Ororo", "Munroe", NA, NA, NA, 2222, "Lake", "f", 1),
   
   # Missing everything except name - no matches
-  create_person(22, "Sumi", "Masimoto", NA, NA, NA, NA, NA, NA, 1),
+  create_person(22, "Piotr", "Rasputin", NA, NA, NA, NA, NA, NA, 1),
   
   # Missing everything except name - matches
-  create_person(23, "Emily", "Hanson", 1964, 04, 19, 430, "University", "f", 1),
-  create_person(23, "Emily", "Hanson", NA, NA, NA, NA, NA, NA, 1),
+  create_person(23, "John", "Proudstar", 1964, 04, 19, 430, "University", "f", 1),
+  create_person(23, "John", "Proudstar", NA, NA, NA, NA, NA, NA, 1),
   
   # Missing everything except address - non-match
   create_person(24, NA, NA, NA, NA, NA, 1610, "Shady Oaks", NA, 1),
